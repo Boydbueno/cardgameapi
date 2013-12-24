e8d9b7cf2ed3d8fcc708dba7c6689b6c312d2571
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-24 20:52:32 +0100

    Changed controller route into manual routing for more control

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,13 @@ Route::group(array('prefix' => 'api'), function()
 	Route::group(array('prefix' => 'v0.1'), function()
 	{
 
-		Route::controller('categories', 'controllers\api\CategoriesController');
+		Route::group(array('prefix' => 'categories'), function()
+		{
+			Route::get('/', 'controllers\api\CategoriesController@getIndex');
+			Route::post('/', 'controllers\api\CategoriesController@postIndex');
+			Route::put('/', 'controllers\api\CategoriesController@putIndex');
+			Route::delete('/', 'controllers\api\CategoriesController@deleteIndex');
+		});
 		
 	});
 
