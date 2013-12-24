2e00e89898fb054f12f341b58be6b1d928ba7007
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-24 20:43:50 +0100

    Added some responses when wrong requests are send to the categories collection

--- app/controllers/api/CategoriesController.php
+++ app/controllers/api/CategoriesController.php
@@ -2,11 +2,38 @@
 
 use Category;
 
+use Response;
+
 class CategoriesController extends \BaseController {
 
 	public function getIndex()
 	{
 		return Category::all();
+	}
+
+	public function postIndex()
+	{
+		// This route will need authorization
+		
+		// If this request is send by a user, it'll be added to the user categories
+		
+		return Response::json(array(
+			'message' => 'It is not possible to create new categories yet. This is a future feature.'
+		), 501);
+	}
+
+	public function deleteIndex()
+	{
+		return Response::json(array(
+			'message' => 'You are not allowed to send a DELETE request to the categories collection. Did you forget to add an id?'
+		), 405);
+	}
+
+	public function putIndex()
+	{
+		return Response::json(array(
+			'message' => 'You are not allowed to send a PUT request to the categories collection. Did you forget to add an id?'
+		), 405);
 	}
 
 }
