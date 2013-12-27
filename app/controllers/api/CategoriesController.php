b3b244ace46577515d57bdbadef090fc80e60c2b
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-27 14:04:36 +0100

    Added some comments and made some changes in the routes

--- app/controllers/api/CategoriesController.php
+++ app/controllers/api/CategoriesController.php
@@ -12,12 +12,12 @@ class CategoriesController extends \BaseController {
 	|---------------------------------------------------------------------------
 	*/
 
-	public function getIndex()
+	public function index()
 	{
 		return Category::all();
 	}
 
-	public function postIndex()
+	public function create()
 	{
 		// TODO: Implement creating new category
 		// This route will need authorization
@@ -27,20 +27,6 @@ class CategoriesController extends \BaseController {
 		return Response::json(array(
 			'message' => 'It is not possible to create new categories yet. This is a future feature.'
 		), 501);
-	}
-
-	public function deleteIndex()
-	{
-		return Response::json(array(
-			'message' => 'You are not allowed to send a DELETE request to the categories collection. Did you forget to add an id?'
-		), 405);
-	}
-
-	public function putIndex()
-	{
-		return Response::json(array(
-			'message' => 'You are not allowed to send a PUT request to the categories collection. Did you forget to add an id?'
-		), 405);
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,41 @@ Route::group(array('prefix' => 'api'), function()
 	Route::group(array('prefix' => 'v0.1'), function()
 	{
 
+		/*
+		|---------------------------------------------------------------------------
+		| Category routes
+		|---------------------------------------------------------------------------
+		*/
+
 		Route::group(array('prefix' => 'categories'), function()
 		{
-			Route::get('/', 'controllers\api\CategoriesController@getIndex');
-			Route::post('/', 'controllers\api\CategoriesController@postIndex');
-			Route::put('/', 'controllers\api\CategoriesController@putIndex');
-			Route::delete('/', 'controllers\api\CategoriesController@deleteIndex');
+
+			/*
+			|---------------------------------------------------------------------------
+			| Category collection routes
+			|---------------------------------------------------------------------------
+			*/
+
+			Route::get('/', 'controllers\api\CategoriesController@index');
+			Route::post('/', 'controllers\api\CategoriesController@create');
+
+			/*
+			|---------------------------------------------------------------------------
+			| Not allowed routes
+			|---------------------------------------------------------------------------
+			*/
+
+			Route::put('/', function() {
+				return Response::json(array(
+					'message' => 'You are not allowed to send a PUT request to the categories collection. Did you forget to add an id?'
+				), 405);
+			});
+
+			Route::delete('/', function() {
+				return Response::json(array(
+					'message' => 'You are not allowed to send a DELETE request to the categories collection. Did you forget to add an id?'
+				), 405);
+			});
 		});
 		
 	});
