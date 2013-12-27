8287e13bdd5250c0873e3f9009775ff4802a63d8
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2013-12-27 23:26:09 +0100

    Added seeder for category question relationship

---
+++ app/database/seeds/CategoryquestionTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+class CategoryquestionTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		DB::table('category_question')->delete();
+
+		$category_question = array(
+			array(
+				'category_id' => Category::whereLabel('Math')->first()->id,
+				'question_id' => Question::whereQuestion('This will be a math question')->first()->id
+			),
+			array(
+				'label' => Category::whereLabel('Science')->first()->id,
+				'question_id' => Question::whereQuestion('This will be a science question')->first()->id
+			),
+			array(
+				'label' => Category::whereLabel('Food')->first()->id,
+				'question_id' => Question::whereQuestion('This will be a food question')->first()->id
+			),
+			array(
+				'label' => Category::whereLabel('History')->first()->id,
+				'question_id' => Question::whereQuestion('This will be a history question')->first()->id
+			)
+		);
+
+		// Uncomment the below to run the seeder
+		DB::table('category_question')->insert($category_question);
+	}
+
+}
