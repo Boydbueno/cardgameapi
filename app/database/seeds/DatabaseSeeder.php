6ccf2e6041079fd889431d7dc391fd3a0373aec4
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-01-12 16:58:24 +0100

    Added a simple user table along with model and seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
-
+		
+		$this->call('UsersTableSeeder');
 		$this->call('CategoriesTableSeeder');
 		$this->call('QuestionsTableSeeder');
 		$this->call('CategoryquestionTableSeeder');
